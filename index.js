50ca217c9301ee9bc27c15498a0c501c1b9d9f42
Yousef Yasser <111029280+franciumm@users.noreply.github.com>
2025-03-03 23:07:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bootstrape from "./src/index.router.js";
 import dotenv from 'dotenv'
 dotenv.config();
 const app = express();
-app.listen( process.env.PORT || 8080, () => {
+app.listen( process.env.PORT || 6900 , () => {
     console.log(`App is running on port ${process.env.PORT } ......`);
 });
 bootstrape(app,express);
