6967f1c02ebccf41a809283c73634b50588e5812
Yousef Yasser <111029280+franciumm@users.noreply.github.com>
2025-03-03 23:10:44 +0200

    Update DB.Connect.js

--- DB/DB.Connect.js
+++ DB/DB.Connect.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 const DBConnect =  ()=>{
     return  mongoose.connect( process.env.MONGOCONNECT, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
         serverSelectionTimeoutMS: 30000, 
     }).then(console.log('DB Connected'));
 }
