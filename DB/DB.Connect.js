6c6d7d70b4ec73f4a5d1de687f16baace8f2bb50
franciumm <111029280+franciumm@users.noreply.github.com>
2025-06-17 06:23:10 +0300

    aa

--- DB/DB.Connect.js
+++ DB/DB.Connect.js
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 const DBConnect =  ()=>{
     return  mongoose.connect( process.env.MONGOCONNECT, {
         serverSelectionTimeoutMS: 30000, 
-      socketTimeoutMS: 45000,        
-      keepAlive: true,              
-      keepAliveInitialDelay: 300000,  
-      connectTimeoutMS: 30000,        
+      connectTimeoutMS: 30000,      
+        socketOptions: {
+        socketTimeoutMS: 45000,         
+        keepAlive: true,                
+        keepAliveInitialDelay: 300000
+      }      
     }).then(console.log('DB Connected'));
 }
 
