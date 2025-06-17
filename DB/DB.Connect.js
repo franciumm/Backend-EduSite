712010aa6bffb022d78105b8d9eab3c6656cd633
franciumm <111029280+franciumm@users.noreply.github.com>
2025-06-17 06:33:50 +0300

    aa

--- DB/DB.Connect.js
+++ DB/DB.Connect.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const DBConnect =  ()=>{
     return  mongoose.connect( process.env.MONGOCONNECT, { // These options are for older Mongoose versions and should be kept.
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      
-      // THIS IS THE MOST IMPORTANT OPTION:
-      // It directly solves the original cold start timeout problem.
+   
       serverSelectionTimeoutMS: 30000,  }).then(console.log('DB Connected'));
 }
 
