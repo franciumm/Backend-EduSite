38a42860c5a816a367a145e050fa1500fe0056a5
franciumm <111029280+franciumm@users.noreply.github.com>
2025-06-20 01:45:12 +0300

    a7ten

--- src/Modules/Exams/controller/Get.js
+++ src/Modules/Exams/controller/Get.js
@@ -7,6 +7,7 @@ import { pagination } from "../../../utils/pagination.js";
 import mongoose from "mongoose";
 import studentModel from "../../../../DB/models/student.model.js";
 import { groupModel } from "../../../../DB/models/groups.model.js";
+import { toZonedTime, fromZonedTime, format } from 'date-fns-tz';
 
 export const getExams = asyncHandler(async (req, res, next) => {
   const { page = 1, size = 10, groupId, gradeId, status } = req.query;
