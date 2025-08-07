03d964611d1a7ed68e5790ae5bbbfeecb06a5ef5
franciumm <111029280+franciumm@users.noreply.github.com>
2025-08-07 08:01:13 +0300

    ALO ASsistant BIg Update Yarab ostor

--- src/Modules/Assistants/assistant.router.js
+++ src/Modules/Assistants/assistant.router.js
@@ -4,13 +4,6 @@ import { AdminAuth } from "../../middelwares/auth.js";
 
 const router = Router();
 
-/**
- * @description This is the most critical security feature of the entire module.
- * `router.use(AdminAuth)` applies the AdminAuth middleware to EVERY route defined
- * below it in this file. This guarantees that no request can even reach the
- * controller functions unless the user has already been verified as a 'main_teacher'.
- */
-router.use(AdminAuth);
 
 // All the following routes are now implicitly protected by AdminAuth.
 router.post('/create', AdminAuth,assistantController.createAssistant);
