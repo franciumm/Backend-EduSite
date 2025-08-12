35934067f2c40c69e3fbe69231895b92113e447a
franciumm <111029280+franciumm@users.noreply.github.com>
2025-08-12 18:17:19 +0300

    ALO ASsistant BIg Update Yarab ostor

--- src/Modules/Assistants/assistant.router.js
+++ src/Modules/Assistants/assistant.router.js
@@ -1,35 +1,27 @@
 import { Router } from "express";
 import * as assistantController from "./controller/assistant.controller.js";
 import { AdminAuth } from "../../middelwares/auth.js";
-import * as validators from "./assistant.validation.js";
-import Joivalidation from "../../middelwares/JoiValidation.js";
 
 const router = Router();
 
 
-// All the following routes are now implicitly protected by AdminAuth.
-router.post('/create',Joivalidation(validators.headers), AdminAuth,Joivalidation(validators.createAssistant),assistantController.createAssistant);
-router.get('/all',Joivalidation(validators.headers), AdminAuth,assistantController.getAllAssistants);
+router.post('/create', AdminAuth,assistantController.createAssistant);
+router.get('/all', AdminAuth,assistantController.getAllAssistants);
 router.get('/:assistantId',
-    Joivalidation(validators.headers),
     AdminAuth,
-     Joivalidation(validators.getOrDeleteAssistantById),
  assistantController.getAssistantById);
 
 
 
  router.put('/:assistantId/permissions',
-        Joivalidation(validators.headers),
 
     AdminAuth, 
-    Joivalidation(validators.updateAssistantPermissions),
     assistantController.updateAssistantPermissions);
 
 
 
-router.delete('/:assistantId',     Joivalidation(validators.headers),
+router.delete('/:assistantId',
 AdminAuth,
-Joivalidation(validators.getOrDeleteAssistantById),
 assistantController.deleteAssistant);
 
 export default router;
