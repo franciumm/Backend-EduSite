845c058c2739e3db1006df699c74ed1171801197
franciumm <111029280+franciumm@users.noreply.github.com>
2025-08-17 02:29:32 +0300

    ALO ASsistant BIg Update Yarab ostor

--- src/Modules/Courses/Courses.router.js
+++ src/Modules/Courses/Courses.router.js
@@ -1,11 +1,7 @@
 import { Router } from "express";
-import { multerCloudFunction } from "../../utils/MulterCloud.js";
-import { allowedExtensions } from "../../utils/allowedExtensions.js";
 
-import { isAuth,AdminAuth } from "../../middelwares/auth.js";
-import { creationValidator } from "../../middelwares/creationValidator.js";
-import { CONTENT_TYPES } from "../../utils/constants.js";
-import { create, GetAll } from "./controller.js";
+import { AdminAuth } from "../../middelwares/auth.js";
+import { create, GetAll ,deleteCourse} from "./controller.js";
 import { createRequestLimiter } from "../../middelwares/ratelimiter.js";
 
 const router = Router();
